dfd3cf94dd3ac35c62b75629ca33abdde69604ca
Opilane TTHK <Katerina2simonova@gmail.com>
2025-12-05 11:09:55 +0200

    ForLoopAdding

---
+++ ForLoopAdding/ForLoopAdding/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ForLoopAdding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Täisarvude kokku liitmine kuni kümneni");
+            Console.WriteLine("--------------------------------------");
+            Console.Write("\n\n");
+
+            int sum = 0;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                sum += i;
+                Console.WriteLine("Summa on: " + sum);
+            }
+        }
+    }
+}
+
+
+
